4b3e092626a49b3c4d1c1c7a19f9e2d438edeb43
truong <truong@gmail.com>
2024-01-13 17:47:21 +0700

    add first

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h1>Hello</h1>
+        </div>
+    </div>
+    
+</body>
+</html>
